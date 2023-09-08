668f17ae23ad874d8869be9d4382657ecfe04611
Divine Adeyeye <87317463+DivineAde@users.noreply.github.com>
2023-09-08 20:19:49 +0100

    changed to utc day

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // array of the 7 days in a week
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const today = new Date();
-const dayOfWeek = daysOfWeek[today.getDay()];
-
-// Displaying the day on the frontend
-document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day of the week: ${dayOfWeek}`;
+function updateCurrentDayOfTheWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date();
+  const dayOfWeek = daysOfWeek[today.getUTCDay()];
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day of the week: ${dayOfWeek}`;
+}
 
 // Function to update the current UTC time
 function updateCurrentUTCTime() {
   const currentDate = new Date();
-  const currentUTCTimeMilliseconds = currentDate.getTime(); 
+  const currentUTCTimeMilliseconds = currentDate.getTime();
   const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
   currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTimeMilliseconds}`;
 }
 
-updateCurrentUTCTime();
+updateCurrentUTCTime(); // Call this function once immediately
+updateCurrentDayOfTheWeek();
